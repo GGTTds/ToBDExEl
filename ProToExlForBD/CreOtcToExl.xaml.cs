10453054ec90989cc1aa517f13ee0c92436e1bde
tpnotllnptl@gmail.com <tpnotllnptl@gmail.com>
2021-03-13 14:24:56 +0500

    #WE-1 Создание основных окон

---
+++ ProToExlForBD/CreOtcToExl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProToExlForBD
+{
+    /// <summary>
+    /// Логика взаимодействия для CreOtcToExl.xaml
+    /// </summary>
+    public partial class CreOtcToExl : Window
+    {
+        public CreOtcToExl()
+        {
+            InitializeComponent();
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ProToExlForBD/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProToExlForBD
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreaOtch_Click(object sender, RoutedEventArgs e)
+        {
+            CreOtcToExl WW = new CreOtcToExl();
+            WW.Show();
+            this.Close();
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
